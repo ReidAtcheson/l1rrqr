e2e78eb7af3755b88470cd81dd2473af44313aa3
Reid Atcheson <reid.atcheson@gmail.com>
2019-12-08 11:56:56 -0600

    added condition number tests

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ This implementation is based off my paper [A Rank Revealing Factorization Using 
 I give the option to use a NAG library solver for the resulting least-1-norm solutions because it is faster and more accurate than
 using a generic linear program solver, but if the module is not found it will default to using the linear program solver in scipy
 instead.
+
+
+
+# Explanation of supplementary files
+
+## Condition number tests
+The file `condition_number_tests.py` shows that the `Q` factor in `AP=QR` has `1-norm` condition number independent of the
+`1-norm` condition number of `A`. This appears to be largely true, but when the conditioning of `A` becomes very large
+the `l1rrqr` algorithm starts to fail. This is likely because it does not have logic for handling the rank deficient case,
+which is fixable.
+
---
+++ condition_number_tests.py
@@ -0,0 +1,29 @@
+import numpy as np
+import scipy.linalg as la
+from l1rrqr import l1rrqr
+
+
+#These tests demonstrate that while the `Q` in `AP=QR` from the `l1rrqr` algorithm
+#is not orthgonal the way it is in a traditional rank-revealing QR, its condition
+#number is still independent of the conditioning of A.
+
+seed=234234
+np.random.seed(seed)
+
+nconds=10
+minorder=5
+maxorder=30
+print("Matrix order,Condition of full matrix,Condition of Q")
+for m in range(minorder,maxorder):
+    for cond in np.logspace(0,nconds,m):
+        #Generate a random matrix with 
+        #prescribed conditioning by working
+        #backwards from an SVD
+        A=np.random.rand(m,m)
+        B=np.random.rand(m,m)
+        U,_=np.linalg.qr(A)
+        V,_=np.linalg.qr(B)
+        D=np.diag(np.linspace(1.0,cond,m))
+        M=U@D@V
+        Q,R,p=l1rrqr(M)
+        print("{},{},{}".format(m,np.linalg.cond(M,p=1),np.linalg.cond(Q,p=1)))
